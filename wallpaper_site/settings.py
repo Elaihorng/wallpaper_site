0dcade76f09603c9253686ad748116aae87f62f2
Elaihorng <elaihorng@gmail.com>
2025-11-20 20:37:42 +0700

    Python

--- wallpaper_site/settings.py
+++ wallpaper_site/settings.py
@@ -178,4 +178,6 @@ CSRF_TRUSTED_ORIGINS = [
     "https://python.pihcwk.space",
 ]
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
